6fa593ef7f85c632b1dccc5a6352f26dc46395d2
Denis Shevchenko <denshn@bk.ru>
2019-10-19 00:44:48 +0300

    Update. 10 pika

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,13 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+
+#define GLM_ENABLE_EXPERIMENTAL
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+
+#include "glm/gtx/string_cast.hpp"
 
 #include <vector>
 #include <string>
@@ -23,6 +27,11 @@
 
 #include "geometry/math_vector.h"
 
+#include <thread>
+#include <chrono>
+
+// using namespace std::chrono_literal;
+
 /// Vertices data ////////////////////////////
 std::vector<GLfloat> vertices = {
     -0.5,   0.5,   0,      1.0f,   0.0f,   0.0f,    0.0f,   1.0f,
@@ -31,33 +40,62 @@ std::vector<GLfloat> vertices = {
     -0.5,   -0.5,   0,     0.0f,   0.0f,   1.0f,    0.0f,   0.0f,
 };
 
-std::vector<GLfloat> vert_cube = {
-    0.0f,0.0f, -1.0f,-1.0f, 1.0f ,
-    1.0f,1.0f,  1.0f, 1.0f, 1.0f ,
-    1.0f,0.0f,  1.0f,-1.0f, 1.0f ,
-    1.0f,0.0f, -1.0f,-1.0f,-1.0f ,
-    0.0f,1.0f, -1.0f, 1.0f, 1.0f ,
-    1.0f,1.0f, -1.0f, 1.0f,-1.0f ,
-    0.0f,1.0f,  1.0f, 1.0f,-1.0f ,
-    0.0f,0.0f,  1.0f,-1.0f,-1.0f ,
-    0.0f,1.0f, -1.0f, 1.0f,-1.0f ,
-    0.0f,0.0f, -1.0f, 1.0f, 1.0f ,
-    1.0f,0.0f,  1.0f, 1.0f, 1.0f ,
-    1.0f,1.0f,  1.0f, 1.0f,-1.0f ,
-    1.0f,1.0f, -1.0f,-1.0f,-1.0f ,
-    0.0f,1.0f,  1.0f,-1.0f,-1.0f ,
-    0.0f,0.0f,  1.0f,-1.0f, 1.0f ,
-    1.0f,0.0f, -1.0f,-1.0f, 1.0f ,
-    1.0f,0.0f,  1.0f,-1.0f,-1.0f ,
-    1.0f,1.0f,  1.0f, 1.0f,-1.0f ,
-    0.0f,1.0f,  1.0f, 1.0f, 1.0f ,
-    0.0f,0.0f,  1.0f,-1.0f, 1.0f ,
-    0.0f,0.0f, -1.0f,-1.0f,-1.0f ,
-    1.0f,0.0f, -1.0f,-1.0f, 1.0f ,
-    1.0f,1.0f, -1.0f, 1.0f, 1.0f ,
-    0.0f,1.0f, -1.0f, 1.0f,-1.0f ,
+GLfloat vert_cube[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+  glm::vec3( 0.0f,  0.0f,  0.0f), 
+  glm::vec3( 2.0f,  5.0f, -15.0f), 
+  glm::vec3(-1.5f, -2.2f, -2.5f),  
+  glm::vec3(-3.8f, -2.0f, -12.3f),  
+  glm::vec3( 2.4f, -0.4f, -3.5f),  
+  glm::vec3(-1.7f,  3.0f, -7.5f),  
+  glm::vec3( 1.3f, -2.0f, -2.5f),  
+  glm::vec3( 1.5f,  2.0f, -2.5f), 
+  glm::vec3( 1.5f,  0.2f, -1.5f), 
+  glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
 
 std::vector<GLfloat> vertices_2 = {
     -0.5,   -0.7,    0,     1.0f,   0.0f,   0.0f,     
@@ -85,8 +123,11 @@ void ChangeMixLevel(float value) {
 }
 
 /// Shaders /////////////////////////////////
-Shader  default_shader, hipno_shader, 
-        shift_shader,   texture_shader;
+Shader  default_shader, 
+        hipno_shader, 
+        shift_shader,   
+        texture_shader;
+        
 GLint vertexColorLocation;
 GLint vertexShiftLocation;
 GLint textureSamplerLocation;
@@ -103,8 +144,8 @@ Texture tex1, tex2;
 /////////////////////////////////////////////
 
 /// Buffers /////////////////////////////////
-GLuint VBOs[3];     // Vertex Buffer Object
-GLuint VAOs[3];     // Vertex Array Object
+GLuint VBO;     // Vertex Buffer Object
+GLuint VAO;     // Vertex Array Object
 GLuint EBO;         // Element Buffer Object
 /////////////////////////////////////////////
 
@@ -115,17 +156,17 @@ GLchar inflog[512];
 
 
 void CreateShaderProgramms() {
-    default_shader  = Shader("../shaders/default.vert", "../shaders/incolor.frag");
-    hipno_shader    = Shader("../shaders/default.vert", "../shaders/uniform_color.frag");
-    shift_shader    = Shader("../shaders/shift.vert", "../shaders/incolor.frag");
+    // default_shader  = Shader("../shaders/default.vert", "../shaders/incolor.frag");
+    // hipno_shader    = Shader("../shaders/default.vert", "../shaders/uniform_color.frag");
+    // shift_shader    = Shader("../shaders/shift.vert", "../shaders/incolor.frag");
 
-    vertexColorLocation = hipno_shader.GetUniformLocation("ourColor2");
+    // vertexColorLocation = hipno_shader.GetUniformLocation("ourColor2");
 
-    vertexShiftLocation = shift_shader.GetUniformLocation("Shift");
+    // vertexShiftLocation = shift_shader.GetUniformLocation("Shift");
 
     texture_shader  = Shader("../shaders/texture.vert", "../shaders/texture.frag");
     // textureSamplerLocation = texture_shader.GetUniformLocation("ourTexture");
-    levelLocation       = texture_shader.GetUniformLocation("level");
+    levelLocation   = texture_shader.GetUniformLocation("level");
     // transformLocation   = texture_shader.GetUniformLocation("transform");
 
     modelLoc        = texture_shader.GetUniformLocation("model");
@@ -141,8 +182,9 @@ void CreateShaderProgramms() {
     
 
 void Init() {
-    glGenVertexArrays(3, VAOs);
-    glGenBuffers(3, VBOs);
+    glEnable(GL_DEPTH_TEST);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
     CreateShaderProgramms();
 
@@ -157,73 +199,96 @@ void Init() {
 
 
 
-void BindBufferData1() {
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices.size(), vertices.data(), GL_STATIC_DRAW);
+// void BindBufferData1() {
+//     glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+//     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*indeces.size(), indeces.data(), GL_STATIC_DRAW);
-}
+//     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*indeces.size(), indeces.data(), GL_STATIC_DRAW);
+// }
 
-void BindBufferData2() {
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices_2.size(), vertices_2.data(), GL_STATIC_DRAW);
+// void BindBufferData2() {
+//     glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+//     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices_2.size(), vertices_2.data(), GL_STATIC_DRAW);
 
-}
+// }
 
 void BindBufferData3() {
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vert_cube.size(), vert_cube.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert_cube), vert_cube, GL_STATIC_DRAW);
 }
 
-void BindVertexAtributes() {
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
-}
+// void BindVertexAtributes() {
+//     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+//     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+//     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+//     glEnableVertexAttribArray(0);
+//     glEnableVertexAttribArray(1);
+//     glEnableVertexAttribArray(2);
+// }
 
-void BindVertexAtributes2() {
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-}
+// void BindVertexAtributes2() {
+//     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+//     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+//     glEnableVertexAttribArray(0);
+//     glEnableVertexAttribArray(1);
+// }
 
 void BindVertexAtributes3() {
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat)));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(1);
 }
 
-void InitVAO() {
-    glBindVertexArray(VAOs[0]);
-        BindBufferData1();
-        BindVertexAtributes();
-    glBindVertexArray(0);
-}
+// void InitVAO() {
+//     glBindVertexArray(VAOs[0]);
+//         BindBufferData1();
+//         BindVertexAtributes();
+//     glBindVertexArray(0);
+// }
 
-void InitVAO2() {
-    glBindVertexArray(VAOs[1]);
-        BindBufferData2();
-        BindVertexAtributes2();
-    glBindVertexArray(0);
-}
+// void InitVAO2() {
+//     glBindVertexArray(VAOs[1]);
+//         BindBufferData2();
+//         BindVertexAtributes2();
+//     glBindVertexArray(0);
+// }
 
 void InitVAO3() {
-    glBindVertexArray(VAOs[2]);
+    glBindVertexArray(VAO);
         BindBufferData3();
         BindVertexAtributes3();
     glBindVertexArray(0);
 }
 
 
+
 void MainDraw() {
+        
+    
+    glm::mat4 view  = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+
+
+
+    
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    
+    projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.1f, 100.0f);
+
+
+
+
+
+
     texture_shader.Use();
-    GLfloat time = glfwGetTime();
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        
+
+    // GLfloat time = glfwGetTime();
 
     /// Uniforms after Use()
 
@@ -246,22 +311,21 @@ void MainDraw() {
     // glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
 
 
-    glm::mat4 model;
-    model = glm::rotate(model, -55.0f, glm::vec3(1.0f,0.0f,0.0f));
-
-    glm::mat4 view;
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    
-    glm::mat4 projection;
-    projection = glm::perspective(45.0f, 800.0f/600.0f, 0.1f, 100.0f);
-
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 
-    glBindVertexArray(VAOs[2]);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+    glBindVertexArray(VAO);
+
+    for(auto i = 0u; i < 10u; ++i) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f,0.0f,0.0f));
+        model = glm::rotate(model, glm::radians((GLfloat)glfwGetTime() * 20.0f * (i+1)), glm::vec3(0.5f, 1.0f, 0.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
@@ -282,19 +346,19 @@ void MainDraw() {
 }
 
 void MainUpdate() {
-    GLfloat time = glfwGetTime();
-    GLfloat redValue    = ((sin(time + (M_PI/3)))+ 1.0) / 2;
-    GLfloat greenValue  = ((sin(time + (2*M_PI/3))) + 1.0) / 2;
-    GLfloat blueValue   = ((sin(time + (3*M_PI/3))) + 1.0) / 2;
+    // GLfloat time = glfwGetTime();
+    // GLfloat redValue    = ((sin(time + (M_PI/3)))+ 1.0) / 2;
+    // GLfloat greenValue  = ((sin(time + (2*M_PI/3))) + 1.0) / 2;
+    // GLfloat blueValue   = ((sin(time + (3*M_PI/3))) + 1.0) / 2;
 
     
-    hipno_shader.Use();
-    glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
-    shift_shader.Use();
-    glUniform2f(vertexShiftLocation, (sin(time))*0.3, 1);
+    // hipno_shader.Use();
+    // glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+    // shift_shader.Use();
+    // glUniform2f(vertexShiftLocation, (sin(time))*0.3, 1);
 
 
-    texture_shader.Use();
+    // texture_shader.Use();
     
 }
 
@@ -348,8 +412,8 @@ int main()
 
     /////////////////////////////////////////
     Init();
-    InitVAO();
-    InitVAO2();
+    // InitVAO();
+    // InitVAO2();
     InitVAO3();
 
     /////////////////////////////////////////
@@ -364,7 +428,7 @@ int main()
         // Render
         // Clear the colorbuffer
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         
         MainDraw();
